1fde8b2a5510d6750ebd182f4c01135aefeef6bf
betulbayram <betulforpc@gmail.com>
2023-02-13 23:36:45 +0300

    for loop

---
+++ for_loops.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int count = 10;
+
+    for (size_t i = 0; i < count; i++)
+    {
+        cout << "i: " << i << endl;
+    }
+    
+
+    return 0;
+}
