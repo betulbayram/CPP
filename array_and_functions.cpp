f38218480c155b62ce44e1fb283d1c98543a4f70
betulbayram <betulforpc@gmail.com>
2023-02-16 18:21:39 +0300

    array as a function parameter

---
+++ array_and_functions.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+
+using namespace std;
+
+void printArray(int array[], int size){
+
+    for (size_t i = 0; i < size; i++)
+    {
+        cout << array[i] << endl;
+    }
+    
+
+}
+
+int main(){
+
+    int a[] = {1,2,3,4,5};
+
+    printArray(a, 5);
+
+    return 0;
+}
