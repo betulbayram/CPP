416074556c93a4eac116f004b7259e402e9248e1
betulbayram <betulforpc@gmail.com>
2023-02-17 00:16:02 +0300

    sizeof operator

---
+++ sizeof.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+    cout << "Integer: " << sizeof(int) << endl;
+    cout << "Char: " << sizeof(char) << endl;
+    cout << "Double: " << sizeof(double) << endl;
+    cout << "Float: " << sizeof(float) << endl;
+
+    return 0;
+
+}
