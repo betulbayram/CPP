26533f9f29444c12fdfc9bc5d8d85e3366202704
betulbayram <betulforpc@gmail.com>
2023-02-21 21:36:50 +0300

    classes

---
+++ Examples/oop/GradeBook/main.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+
+using namespace std;
+
+class GradeBook{
+
+    public:
+        void displayMessage(){
+
+            cout << "Welcome to the Grade Book!" << endl;
+
+        }
+
+};
+
+
+int main(){
+
+    GradeBook myGradeBook;
+    myGradeBook.displayMessage();
+
+    return 0;
+}
